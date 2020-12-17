e0517a1dabb75b91fbc344dc0368a99da0c57ed8
Vikas William <vikasdenobili@outlook.com>
2020-12-17 00:23:54 +0000

    a

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText]= useState("");
+  const [todos,setTodos]= useState([]);
   return (
     <div className="App">
       <header>
-        <h3>Vikas TODO LISt</h3>
+        <h3>Vikas TODO LIST</h3>
       </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList/>
     </div>
   );
 }
